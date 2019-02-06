a0b979ab20c25e2b84820a8304ac7c36bb4d7cce
Seris02200059 <38371667+Seris02200059@users.noreply.github.com>
2019-02-06 13:31:39 +0800

    Add files via upload

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,24 +1,78 @@
 /**
- * Name         : Yu Chenghui
- * Matric. No   : A0194474U
- * PLab Acct.   :
+ *
+ * author	: Yu Chenghui
+ * matric no: A0194474U
+ *
  */
 
 import java.util.*;
 
 public class HelloWorld {
-	private void run() {
-		Scanner sc = new Scanner(System.in);
-		String word = sc.nextLine();
-		if (word.equals("Hello World")) {
-			System.out.println("Correct");
-		} else {
-			System.out.println("Incorrect");
-		}
-	}
 
 	public static void main(String[] args) {
-		HelloWorld newHelloWorld = new HelloWorld();
-		newHelloWorld.run();
+
+		// declare the necessary variables
+    int method;
+		String type;
+		int i;
+		int j;
+
+		// declare a Scanner object to read input
+    Scanner scanner = new Scanner(System.in);
+
+		// read input and process them accordingly
+    method = scanner.nextInt();
+
+		// method 1
+		if (method == 1) {
+			int num = scanner.nextInt();
+			for (int count = 0; count < num; count++) {
+				type = scanner.next();
+    		i = scanner.nextInt();
+				j = scanner.nextInt();
+    		if (type.equals("AND")) {
+					System.out.println(i * j);
+				} else {
+					System.out.println((i + j == 0) ? 0 : 1);
+				}
+			}
+		} else if (method == 2) {
+			// method 2
+
+			boolean flag2 = false;
+			boolean flag = scanner.hasNextInt();
+			if (flag) {
+				int number = scanner.nextInt();
+			  flag2 = (number == -1 || number == 0);
+			}
+
+			while (!flag2) {
+				type = scanner.next();
+				i = scanner.nextInt();
+				j = scanner.nextInt();
+				if (type.equals("AND")) {
+				 	System.out.println(i * j);
+			 	} else {
+				 	System.out.println((i + j == 0) ? 0 : 1);
+			 	}
+				flag = scanner.hasNextInt();
+				if (flag) {
+					int number2 = scanner.nextInt();
+					flag2 = (number2 == -1 || number2 == 0);
+				}
+			}
+		} else {
+			// method 3
+			while (scanner.hasNext()) {
+				type = scanner.next();
+				i = scanner.nextInt();
+				j = scanner.nextInt();
+				if (type.equals("AND")) {
+				 	System.out.println(i * j);
+			 	} else {
+				 	System.out.println((i + j == 0) ? 0 : 1);
+			 	}
+			}
+		}
 	}
 }
